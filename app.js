6fa4c3de41e6bc36ad38019e5e83e7bb299f5e58
Litpax-Technology <mis@litpaxtechnology.com>
2026-08-11 12:32:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwKxl2zKmbbfHnvgTlIHkyRfz54yYA0mopQHRf3zoSR5jFzLJgegQVwNQ7xDlPq8g0/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyE7B3jPUf6zXDG8U5a1hQLdK_v1TpePRC0WLKG3UGW56-a83oEPb-NTwd3Yx4nhBgp/exec";
 
 const form       = document.getElementById("supportForm");
 const submitBtn  = document.getElementById("submitBtn");
